01892f55035955f71bbd425be33a260ab31d8a12
Shannen Grimes <shannengrimes@yahoo.com>
2019-11-12 17:25:12 -0600

    Adding final styling changes.  Still need to shrink the logo photo.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -38,8 +38,8 @@ $(document).ready(function(){
         $("questions").empty();
         $("#timer").empty();
         $("#timer").hide();
-        $("#correct-answers").text("Correct answers (You Rock!): " + numCorrect);
-        $("#incorrect-answers").text("Incorrect answers (Better luck rockin next time!): " + numIncorrect);
+        $("#correctAnswers").text("You Rock!  Here are the answers that were correct: " + numCorrect);
+        $("#incorrectAnswers").text("Incorrect answers (Better luck rockin next time!): " + numIncorrect);
         $("#unanswered").text("Skipped questions (You don't seem much of a rocker!): " + numUnanswered);
     }
   }
@@ -51,7 +51,7 @@ $(document).ready(function(){
           divContainer.append('<h2>Answer the following questions to test your Rock skills:</h2>');
 
           for (var i = 0; i < questionTally.length; i++) {
-              divContainer.append('<div id="question">' + questionTally[i].question + '</div>');
+              divContainer.append('<div id="questions">' + questionTally[i].questions + '</div>');
               var answer1 = questionTally[i].answers[0];
               var answer2 = questionTally[i].answers[1];
               var answer3 = questionTally[i].answers[2];
@@ -73,7 +73,7 @@ $(document).ready(function(){
           var numUnanswered = 0;
           for (var i = 0; i < questionTally.length; i++) {
               correctAnswer = questionTally[i].correct;
-              useranswer = $('input[id=radio'+i+']:checked + label').text();
+              userAnswer = $('input[id=radio'+i+']:checked + label').text();
               if (userAnswer === correctAnswer) {
                 numCorrect++;
               } else if (userAnswer === "") {
@@ -96,37 +96,37 @@ $(document).ready(function(){
   var questionTally = 
   [
       {
-          question: "Bands Soundgarden, Alice In Chains and Pearl Jam are from what American city?",
+          questions: "Bands Soundgarden, Alice In Chains and Pearl Jam are from what American city?",
           answers: ["Spokane","Seattle","Portland"],
           correct: "Seattle"
       },
       {
-          question: "Vocalist and guitarist Jimi Hendrix served in which unit military unit?",
+          questions: "Vocalist and guitarist Jimi Hendrix served in which unit military unit?",
           answers: ["101st Airborne Division", "7th Special Forces Group", "United States Army Band"],
           correct: "101st Airborne Division"
       },
       {
-          question: "Two of these groups are tied for 1st with 3 tours in the in the Top 20 of all time highest grossing tours. Which band only has 2 in the Top 20?",
+          questions: "Two of these groups are tied for 1st with 3 tours in the in the Top 20 of all time highest grossing tours. Which band only has 2 in the Top 20?",
           answers: ["Rollings Stones", "U2", "Bruce Springsteen and the E Street Band"],
           correct: "Bruce Springsteen and the E Street Band"
       },
       {
-          question: "In 2019, which festival changed it's name to Sonic Temple?",
+          questions: "In 2019, which festival changed it's name to Sonic Temple?",
           answers: ["Louder Than Life, Louisville, KY", "Rock on the Range, Columbus, OH", "Rock USA, Oshkosh, WI"],
           correct: "Rock on the Range, Columbus, OH"
       },      
       {
-          question: "Of the top 3 albums of all time worldwide in certified sales, which is the highest?",
+          questions: "Of the top 3 albums of all time worldwide in certified sales, which is the highest?",
           answers: ["Thriller, Michael Jackson", "Eagles: Their Greatest Hits 1971-1975", "Hotel California, Eagles"],
           correct: "Thriller, Michael Jackson"
       },      
       {
-          question: "Dave Grohl had a designed a throne to play from after breaking his leg when he fell off a stage while performing. Who did he let use his throne for concerts after suffering a foot injury?",
+          questions: "Dave Grohl had a designed a throne to play from after breaking his leg when he fell off a stage while performing. Who did he let use his throne for concerts after suffering a foot injury?",
           answers: ["Axl Rose", "Chester Bennington", "James Hatfield"],
           correct: "Axl Rose"
       },      
       {
-          question: "Who has not been a lead singer for Black Sabbath?",
+          questions: "Who has not been a lead singer for Black Sabbath?",
           answers: ["Ozzy Osborne", "Ronnie James Dio", "Tommy Lommi"],
           correct: "Tommy Lommi"
       },      
@@ -136,12 +136,12 @@ $(document).ready(function(){
           correct: "Trumpet"
       },      
       {
-          question: "What band's previous names was The Tea Set and The Abdabs?",
+          questions: "What band's previous names was The Tea Set and The Abdabs?",
           answers: ["Cream", "The Who", "Pink Floyd"],
           correct: "Pink Floyd"
       },      
       {
-          question: "Guitarists Angus and Malcolm Young of the Australian band AC/DC were born where?",
+          questions: "Guitarists Angus and Malcolm Young of the Australian band AC/DC were born where?",
           answers: ["New Zealand", "Scotland", "Wales"],
           correct: "Scotland"
       }
