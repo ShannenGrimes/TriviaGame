6cbb15bdba3a4a7a53da86b8e3d604361b44e0f4
Shannen Grimes <shannengrimes@yahoo.com>
2019-11-12 13:23:57 -0600

    Added timer and logic for answers

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,6 +26,30 @@ $(document).ready(function(){
         }
     },
 
-        
+    // Stop the timer and chck the answers
+    stopTimer: function() {
+        clearInterval();
+        trivia.checkAnswers();
+    },
+
+    // Display results
+    showEndPage: function(numCorrect, numIncorrect, numUnanswered) {
+        $("#end").show();
+        $("questions").empty();
+        $("#timer").empty();
+        $("#timer").hide();
+        $("#correct-answers").text("Correct answers (You Rock!): " + numCorrect);
+        $("#incorrect-answers").text("Incorrect answers (Better luck rockin next time!): " + numIncorrect);
+        $("#unanswered").text("Skipped questions (You don't seem much of a rocker!): " + numUnanswered);
+    }
+  }
+//   Logic of looping through the questions and appending them
+  var trivia = {
+      displayQuestions: function() {
+          var divCounter = $("#questions");
+          var answerGroup = $("form-check");
+          divContainer.append('<h2>Answer the following questions to test your Rock skills:</h2>');
+          
+      }
 
   }
